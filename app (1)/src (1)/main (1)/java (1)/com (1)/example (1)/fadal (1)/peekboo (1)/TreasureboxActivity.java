2e55d70e75817605e32de04c97eee90cee117827
monermon <mmlee15@yonsei.ac.kr>
2018-11-22 06:27:07 +0900

    Initial commit

---
+++ app (1)/src (1)/main (1)/java (1)/com (1)/example (1)/fadal (1)/peekboo (1)/TreasureboxActivity.java
@@ -0,0 +1,13 @@
+package com.example.fadal.peekboo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TreasureboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treasurebox);
+    }
+}
---
+++ app (1)/src (1)/main (1)/java (1)/com (1)/example (1)/fadal (1)/peekboo (1)/TypesActivity.java
@@ -0,0 +1,13 @@
+package com.example.fadal.peekboo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TypesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_types);
+    }
+}
